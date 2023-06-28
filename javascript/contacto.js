b876131fcf1e521838a076dad56e9f5b664a6fd1
bleeda097 <104221487+jaguarRubio@users.noreply.github.com>
2023-06-28 11:17:43 -0300

    Contact validation colors and button

--- javascript/contacto.js
+++ javascript/contacto.js
@@ -6,20 +6,23 @@ id = getURL.get('id')
 console.log(id)
 
 const url = 'https://jsonplaceholder.typicode.com/users'
+if(id != null){ //if it catch value: print
 fetch(`${url}/${id}`)
-.then(res => res.json())
-.then(data => {
-    const name = document.createElement('p')
-    const email = document.createElement('p')
-    const phone= document.createElement('p')
-    name.innerHTML = data.name
-    email.innerHTML = data.email
-    phone.innerHTML = data.phone
-    aplicacion.appendChild(name)
-    aplicacion.appendChild(email)
-    aplicacion.appendChild(phone)
-})
-.catch(err => console.log(err))
+
+    .then(res => res.json())
+    .then(data => {
+        const name = document.createElement('p')
+        const email = document.createElement('p')
+        const phone= document.createElement('p')
+        name.innerHTML = data.name
+        email.innerHTML = data.email
+        phone.innerHTML = data.phone
+        aplicacion.appendChild(name)
+        aplicacion.appendChild(email)
+        aplicacion.appendChild(phone)
+    })
+    .catch(err => console.log(err))
+}
 
 
 
@@ -35,23 +38,31 @@ function validator(){
     var flag = false;
     var cont = 0;
 
-
+    var arti = document.querySelectorAll('form fieldset article');
+    var redMsg = '#ff0000'; //it will be added VUE interactive real time it'll be far better.
+    var btnError = '#999900';
 
     const msgError = document.getElementById("error0");
 
     if(Name.length < 2 || Name.length >= 33){
         msgError.innerHTML = "Incorrect. send an inteligible name."
+        msgError.style.backgroundColor = redMsg;
+        arti[0].style.backgroundColor = redMsg;
     }else{
         msgError.innerHTML = "Correct. it's inteligible name."
         flag = true
         cont += 1;
     };
 
-    const electronicMail = camp[2].value;
+
+
+    const electronicMail = camp[2].value; 
     const ErrorMail = document.getElementById("error1");
     if (electronicMail.indexOf("@", 0) == -1){
         ErrorMail.innerHTML = "Error. Mail hasn't '@' invalid."
         var flag = false
+        ErrorMail.style.backgroundColor = redMsg;
+        arti[1].style.backgroundColor = redMsg;
     }else{
         ErrorMail.innerHTML = "Valid. Mail has '@'."
         flag = true
@@ -63,6 +74,8 @@ function validator(){
     if(LrgMessage.length <= 1){
         ErrorLrg.innerHTML = "Negative, the message is too short."
         var flag = false
+        ErrorLrg.style.backgroundColor = redMsg;
+        arti[2].style.backgroundColor = redMsg;
     }else{
         ErrorLrg.innerHTML= "Affirmative."
         flag = true
@@ -70,10 +83,18 @@ function validator(){
     }
 
     const sending = document.getElementById("sent");
+    var btn = document.querySelector('button')
+    var fieldset = document.querySelector('fieldset')
+
     if(flag && cont == 3){
-        sending.innerHTML = "Sent righfully" 
+        sending.innerHTML = "Sent righfully"
     }else{
         sending.innerHTML = "Couldn't send"
+        sending.style.backgroundColor = redMsg;
+        arti[3].style.backgroundColor = redMsg;
+        btn.style.backgroundColor = btnError;
+        btn.style.animation = 'none';
+        fieldset.style.gap = '4px 0px';
     }
 
     // if(x.indexOf("@") == -1){
